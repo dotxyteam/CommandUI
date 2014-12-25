2a8f96d2b411d70b95ffb3572c244bd08d243ef2
olitank <olitank@yahoo.fr>
2014-12-25 22:26:23 +0100

    update

---
+++ command-ui/tmp-src/Test.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+import org.jdesktop.swingx.JXBusyLabel;
+
+
+public class Test {
+	
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, new JXBusyLabel());
+	}
+
+}
