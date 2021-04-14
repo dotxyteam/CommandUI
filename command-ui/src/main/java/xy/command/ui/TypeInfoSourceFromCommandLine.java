90e75e4d041a1e058ccba2f6d239708e43a5c75a
olitank <olitank@olitank-HP-Laptop-15-da0xxx>
2021-04-14 20:01:21 +0200

    Moved the execute method to the bottom of the dialog.

--- command-ui/src/main/java/xy/command/ui/TypeInfoSourceFromCommandLine.java
+++ command-ui/src/main/java/xy/command/ui/TypeInfoSourceFromCommandLine.java
@@ -1,12 +1,8 @@
 package xy.command.ui;
 
-import java.awt.Dimension;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.SwingUtilities;
 
@@ -24,278 +20,20 @@ import xy.command.model.MultiplePart;
 import xy.command.model.OptionalPart;
 import xy.command.ui.util.CommandUIUtils;
 import xy.reflect.ui.ReflectionUI;
-import xy.reflect.ui.info.ColorSpecification;
-import xy.reflect.ui.info.InfoCategory;
-import xy.reflect.ui.info.ResourcePath;
+import xy.reflect.ui.info.field.FieldInfoProxy;
 import xy.reflect.ui.info.field.IFieldInfo;
-import xy.reflect.ui.info.menu.MenuModel;
-import xy.reflect.ui.info.method.AbstractConstructorInfo;
 import xy.reflect.ui.info.method.IMethodInfo;
 import xy.reflect.ui.info.method.InvocationData;
 import xy.reflect.ui.info.method.MethodInfoProxy;
+import xy.reflect.ui.info.type.DefaultTypeInfo;
 import xy.reflect.ui.info.type.ITypeInfo;
 import xy.reflect.ui.info.type.source.ITypeInfoSource;
+import xy.reflect.ui.info.type.source.JavaTypeInfoSource;
 import xy.reflect.ui.info.type.source.SpecificitiesIdentifier;
 import xy.reflect.ui.util.ReflectionUIError;
 import xy.reflect.ui.util.ReflectionUIUtils;
 
 public class TypeInfoSourceFromCommandLine implements ITypeInfoSource {
-
-	public class TypeInfoFromCommandLine implements ITypeInfo {
-
-		private ReflectionUI reflectionUI;
-
-		public TypeInfoFromCommandLine(ReflectionUI reflectionUI) {
-			this.reflectionUI = reflectionUI;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + getOuterType().hashCode();
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			TypeInfoFromCommandLine other = (TypeInfoFromCommandLine) obj;
-			if (!getOuterType().equals(other.getOuterType()))
-				return false;
-			return true;
-		}
-
-		@Override
-		public Map<String, Object> getSpecificProperties() {
-			return Collections.emptyMap();
-		}
-
-		@Override
-		public String getOnlineHelp() {
-			return null;
-		}
-
-		@Override
-		public String getName() {
-			return commandLine.getClass().getName() + commandLine.hashCode();
-		}
-
-		@Override
-		public String getCaption() {
-			return commandLine.title;
-		}
-
-		@Override
-		public void validate(Object object) throws Exception {
-		}
-
-		@Override
-		public String toString(Object object) {
-			return object.toString();
-		}
-
-		@Override
-		public boolean supportsInstance(Object object) {
-			return (object instanceof CommandLineInstance) && (((CommandLineInstance) object).model == commandLine);
-		}
-
-		@Override
-		public void save(Object object, OutputStream out) {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
-		public boolean onFormVisibilityChange(Object object, boolean visible) {
-			return false;
-		}
-
-		@Override
-		public void load(Object object, InputStream in) {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
-		public boolean isPrimitive() {
-			return false;
-		}
-
-		@Override
-		public boolean isModificationStackAccessible() {
-			return true;
-		}
-
-		@Override
-		public boolean isImmutable() {
-			return false;
-		}
-
-		@Override
-		public boolean isConcrete() {
-			return true;
-		}
-
-		@Override
-		public ITypeInfoSource getSource() {
-			return TypeInfoSourceFromCommandLine.this;
-		}
-
-		@Override
-		public List<ITypeInfo> getPolymorphicInstanceSubTypes() {
-			return Collections.emptyList();
-		}
-
-		@Override
-		public MethodsLayout getMethodsLayout() {
-			return MethodsLayout.HORIZONTAL_FLOW;
-		}
-
-		@Override
-		public List<IMethodInfo> getMethods() {
-			if (commandLine instanceof CommandLineProject) {
-				List<IMethodInfo> result = new ArrayList<IMethodInfo>();
-				result.add(getExecutionMethod(reflectionUI));
-				return result;
-			} else {
-				return Collections.emptyList();
-			}
-		}
-
-		@Override
-		public MenuModel getMenuModel() {
-			return new MenuModel();
-		}
-
-		@Override
-		public ResourcePath getIconImagePath() {
-			return null;
-		}
-
-		@Override
-		public Dimension getFormPreferredSize() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormForegroundColor() {
-			return null;
-		}
-
-		@Override
-		public ResourcePath getFormBackgroundImagePath() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormBackgroundColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormBorderColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormEditorsForegroundColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormEditorsBackgroundColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormButtonBackgroundColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormButtonForegroundColor() {
-			return null;
-		}
-
-		@Override
-		public ResourcePath getFormButtonBackgroundImagePath() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getFormButtonBorderColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getCategoriesBackgroundColor() {
-			return null;
-		}
-
-		@Override
-		public ColorSpecification getCategoriesForegroundColor() {
-			return null;
-		}
-
-		@Override
-		public FieldsLayout getFieldsLayout() {
-			return FieldsLayout.VERTICAL_FLOW;
-		}
-
-		@Override
-		public List<IFieldInfo> getFields() {
-			List<IFieldInfo> result = new ArrayList<IFieldInfo>();
-			for (ArgumentPage argumentPage : commandLine.arguments) {
-				for (AbstractCommandLinePart part : argumentPage.parts) {
-					IFieldInfo field = getFieldInfoFromCommandLinePart(reflectionUI, part, argumentPage, this);
-					if (field == null) {
-						continue;
-					}
-					result.add(field);
-				}
-			}
-			return result;
-		}
-
-		@Override
-		public List<IMethodInfo> getConstructors() {
-			return Collections.<IMethodInfo>singletonList(new AbstractConstructorInfo() {
-
-				@Override
-				public Object invoke(Object parentObject, InvocationData invocationData) {
-					return new CommandLineInstance(commandLine);
-				}
-			});
-		}
-
-		@Override
-		public CategoriesStyle getCategoriesStyle() {
-			return CategoriesStyle.MODERN;
-		}
-
-		@Override
-		public Object copy(Object object) {
-			throw new UnsupportedOperationException();
-		}
-
-		@Override
-		public boolean canPersist() {
-			return false;
-		}
-
-		@Override
-		public boolean canCopy(Object object) {
-			return false;
-		}
-
-		private TypeInfoSourceFromCommandLine getOuterType() {
-			return TypeInfoSourceFromCommandLine.this;
-		}
-	}
 
 	private CommandLine commandLine;
 	private SpecificitiesIdentifier specificitiesIdentifier;
@@ -303,54 +41,6 @@ public class TypeInfoSourceFromCommandLine implements ITypeInfoSource {
 	public TypeInfoSourceFromCommandLine(CommandLine commandLine, SpecificitiesIdentifier specificitiesIdentifier) {
 		this.commandLine = commandLine;
 		this.specificitiesIdentifier = specificitiesIdentifier;
-	}
-
-	public IMethodInfo getExecutionMethod(ReflectionUI reflectionUI) {
-		return new MethodInfoProxy(IMethodInfo.NULL_METHOD_INFO) {
-
-			@Override
-			public String getName() {
-				return "execute";
-			}
-
-			@Override
-			public String getSignature() {
-				return ReflectionUIUtils.buildMethodSignature(this);
-			}
-
-			@Override
-			public String getCaption() {
-				return "Execute";
-			}
-
-			@Override
-			public InfoCategory getCategory() {
-				return new InfoCategory("Finish", Integer.MAX_VALUE, null);
-			}
-
-			@Override
-			public boolean isReadOnly() {
-				return true;
-			}
-
-			@Override
-			public Object invoke(Object object, InvocationData invocationData) {
-				final CommandLineProject project = (CommandLineProject) commandLine;
-				final CommandLineInstance instance = (CommandLineInstance) object;
-				final String commandText = CommandUIUtils.quoteArgument(project.executablePath.getPath()) + " "
-						+ instance.getExecutionText();
-				SwingUtilities.invokeLater(new Runnable() {
-					@Override
-					public void run() {
-						CommandMonitoringDialog d = new CommandMonitoringDialog(null, commandText,
-								project.executionDir);
-						d.setVisible(true);
-					}
-				});
-				return null;
-			}
-
-		};
 	}
 
 	@Override
@@ -391,7 +81,7 @@ public class TypeInfoSourceFromCommandLine implements ITypeInfoSource {
 
 	@Override
 	public ITypeInfo getTypeInfo(ReflectionUI reflectionUI) {
-		return new TypeInfoFromCommandLine(reflectionUI);
+		return new TypeInfo(reflectionUI);
 	}
 
 	protected IFieldInfo getFieldInfoFromCommandLinePart(ReflectionUI reflectionUI, AbstractCommandLinePart part,
@@ -417,6 +107,225 @@ public class TypeInfoSourceFromCommandLine implements ITypeInfoSource {
 					commandLineTypeInfo);
 		} else {
 			throw new ReflectionUIError();
+		}
+	}
+
+	protected class TypeInfo extends DefaultTypeInfo {
+
+		public TypeInfo(ReflectionUI reflectionUI) {
+			super(reflectionUI, new JavaTypeInfoSource(CommandLineInstance.class, null));
+		}
+
+		@Override
+		public String getCaption() {
+			return commandLine.title;
+		}
+
+		@Override
+		public List<IMethodInfo> getMethods() {
+			List<IMethodInfo> result = new ArrayList<IMethodInfo>();
+			result.add(new ExecutionMethodInfo(reflectionUI));
+			if ((commandLine.description != null) && (commandLine.description.trim().length() > 0)) {
+				result.add(new HelpMethodInfo(reflectionUI));
+			}
+			return result;
+		}
+
+		@Override
+		public List<IFieldInfo> getFields() {
+			return Collections.singletonList(new CapsuleFieldInfo(reflectionUI));
+		}
+
+	}
+
+	protected class Capsule {
+
+		protected CommandLineInstance commandLineInstance;
+
+		public Capsule(CommandLineInstance commandLineInstance) {
+			super();
+			this.commandLineInstance = commandLineInstance;
+		}
+
+		public CommandLineInstance getCommandLineInstance() {
+			return commandLineInstance;
+		}
+
+	}
+
+	protected class CapsuleFieldInfo extends FieldInfoProxy {
+
+		protected ReflectionUI reflectionUI;
+
+		public CapsuleFieldInfo(ReflectionUI reflectionUI) {
+			super(IFieldInfo.NULL_FIELD_INFO);
+			this.reflectionUI = reflectionUI;
+		}
+
+		@Override
+		public String getName() {
+			return "capsule";
+		}
+
+		@Override
+		public String getCaption() {
+			return "";
+		}
+
+		@Override
+		public void setValue(Object object, Object value) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean isFormControlMandatory() {
+			return true;
+		}
+
+		@Override
+		public boolean isFormControlEmbedded() {
+			return true;
+		}
+
+		@Override
+		public Object getValue(Object object) {
+			Capsule result = new Capsule((CommandLineInstance) object);
+			reflectionUI.registerPrecomputedTypeInfoObject(result, new CapsuleTypeInfo(reflectionUI));
+			return result;
+		}
+
+		@Override
+		public ITypeInfo getType() {
+			return new CapsuleTypeInfo(reflectionUI);
+		}
+
+	}
+
+	protected class CapsuleTypeInfo extends DefaultTypeInfo {
+
+		public CapsuleTypeInfo(ReflectionUI reflectionUI) {
+			super(reflectionUI, new JavaTypeInfoSource(Capsule.class, new SpecificitiesIdentifier(
+					new TypeInfo(reflectionUI).getName(), new CapsuleFieldInfo(reflectionUI).getName())));
+		}
+
+		@Override
+		public List<IMethodInfo> getMethods() {
+			return Collections.emptyList();
+		}
+
+		@Override
+		public List<IFieldInfo> getFields() {
+			List<IFieldInfo> result = new ArrayList<IFieldInfo>();
+			for (ArgumentPage argumentPage : commandLine.arguments) {
+				for (AbstractCommandLinePart part : argumentPage.parts) {
+					IFieldInfo field = getFieldInfoFromCommandLinePart(reflectionUI, part, argumentPage, this);
+					if (field == null) {
+						continue;
+					}
+					field = new FieldInfoProxy(field) {
+
+						@Override
+						public Object getValue(Object object) {
+							return super.getValue(((Capsule) object).getCommandLineInstance());
+						}
+
+						@Override
+						public void setValue(Object object, Object value) {
+							super.setValue(((Capsule) object).getCommandLineInstance(), value);
+						}
+
+					};
+					result.add(field);
+				}
+			}
+			return result;
+		}
+
+	}
+
+	protected class ExecutionMethodInfo extends MethodInfoProxy {
+
+		protected ReflectionUI reflectionUI;
+
+		public ExecutionMethodInfo(ReflectionUI reflectionUI) {
+			super(IMethodInfo.NULL_METHOD_INFO);
+			this.reflectionUI = reflectionUI;
+		}
+
+		@Override
+		public String getName() {
+			return "execute";
+		}
+
+		@Override
+		public String getSignature() {
+			return ReflectionUIUtils.buildMethodSignature(this);
+		}
+
+		@Override
+		public String getCaption() {
+			return "Execute";
+		}
+
+		@Override
+		public boolean isReadOnly() {
+			return true;
+		}
+
+		@Override
+		public Object invoke(Object object, InvocationData invocationData) {
+			final CommandLineProject project = (CommandLineProject) commandLine;
+			final CommandLineInstance instance = (CommandLineInstance) object;
+			final String commandText = CommandUIUtils.quoteArgument(project.executablePath.getPath()) + " "
+					+ instance.getExecutionText();
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					CommandMonitoringDialog d = new CommandMonitoringDialog(null, commandText, project.executionDir);
+					d.setVisible(true);
+				}
+			});
+			return null;
+		}
+	}
+
+	protected class HelpMethodInfo extends MethodInfoProxy {
+
+		protected ReflectionUI reflectionUI;
+
+		public HelpMethodInfo(ReflectionUI reflectionUI) {
+			super(IMethodInfo.NULL_METHOD_INFO);
+			this.reflectionUI = reflectionUI;
+		}
+
+		@Override
+		public String getName() {
+			return "showHelp";
+		}
+
+		@Override
+		public String getSignature() {
+			return ReflectionUIUtils.buildMethodSignature(this);
+		}
+
+		@Override
+		public String getCaption() {
+			return "Show Help";
+		}
+
+		@Override
+		public boolean isReadOnly() {
+			return true;
+		}
+
+		@Override
+		public ITypeInfo getReturnValueType() {
+			return reflectionUI.getTypeInfo(new JavaTypeInfoSource(String.class, null));
+		}
+
+		@Override
+		public Object invoke(Object object, InvocationData invocationData) {
+			return commandLine.description;
 		}
 	}
 
