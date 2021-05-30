c7f5c8f7c414366a36c45c1d6c4d2f97fd19ba46
olitank <olitank@olitank-HP-Laptop-15-da0xxx>
2021-05-30 23:41:22 +0200

    junit test added

---
+++ command-ui/src/test/java/xy/command/ui/TestWithAutomation.java
@@ -0,0 +1,53 @@
+package xy.command.ui;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import xy.reflect.ui.util.MiscUtils;
+import xy.reflect.ui.util.MoreSystemProperties;
+import xy.ui.testing.Tester;
+import xy.ui.testing.util.TestingUtils;
+
+public class TestWithAutomation {
+
+	Tester tester = new Tester();
+
+	protected static void checkSystemProperty(String key, String expectedValue) {
+		String value = System.getProperty(key);
+		if (!MiscUtils.equalsOrBothNull(expectedValue, value)) {
+			String errorMsg = "System property invalid value:\n" + "-D" + key + "=" + value + "\nExpected:\n" + "-D"
+					+ key + "=" + expectedValue;
+			System.err.println(errorMsg);
+			throw new AssertionError(errorMsg);
+
+		}
+	}
+
+	public static void setupTestEnvironment() {
+		checkSystemProperty(MoreSystemProperties.DEBUG, "true");
+	}
+
+	@BeforeClass
+	public static void beforeAllTests() {
+		setupTestEnvironment();
+		TestingUtils.purgeAllReportsDirectory();
+	}
+
+	@Test
+	public void test() throws Exception {
+		File comanndLineSpecFile = new File("test.cml");
+		if (comanndLineSpecFile.exists()) {
+			Files.delete(comanndLineSpecFile.toPath());
+		}
+		try {
+			TestingUtils.assertSuccessfulReplay(tester, new File(
+					System.getProperty("command-ui-tests.project.directory", "./") + "test-specifications/test.stt"));
+		} finally {
+			Files.delete(comanndLineSpecFile.toPath());
+		}
+	}
+
+}
